dc8672df7dc7d7076db8cfa80b8f2f31ad711113
Matt <54966468+mattdholmes@users.noreply.github.com>
2019-09-19 22:06:27 -0400

    Win Text

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour{
     public float speed;
+    public Text countText;
+    public Text winText;
     private Rigidbody rb;
+    private int count;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void FixedUpdate()
     {
@@ -21,7 +28,17 @@ public class PlayerController : MonoBehaviour{
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
 
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 14){
+            winText.text = "You Win!";
+
+        }
     }
 }
