3be85386f16321f3d2902b5c482fabccb04ceb1f
Matt <54966468+mattdholmes@users.noreply.github.com>
2019-09-19 21:05:36 -0400

    Pickup part 1

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{ private Rigidbody rb;
+public class PlayerController : MonoBehaviour{
+    public float speed;
+    private Rigidbody rb;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -13,6 +14,14 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+
     }
 }
